a60b3ba2cc099a65c2d7d61fc8ccbbffe155dee6
edsx <eduardodsxavier@gmail.com>
2024-12-05 18:15:28 -0300

    made pass a path to a file or dir with -p

--- withNode/bin/index.ts
+++ withNode/bin/index.ts
@@ -1,4 +1,14 @@
 #! /usr/bin/env node
 const yargs = require("yargs");
 
-console.log("args: " + yargs.argv._);
+const usage = "\nUsage: tran <file_path> file to be used";
+const options = yargs  
+.usage(usage)  
+.option("p", { alias:"path", describe: "Path to file", type: "String", demandOption: true })                                                                                                    
+.help(true)  
+.argv;
+
+
+const path = process.cwd() + "/" + yargs.argv.p
+
+console.log(path);
