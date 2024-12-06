abbfabce1eb60cdf58a15fba4c1aa3d294601164
edsx <eduardodsxavier@gmail.com>
2024-12-06 19:19:16 -0300

    show status of file

--- withNode/bin/index.ts
+++ withNode/bin/index.ts
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 const yargs = require("yargs");
+const fs = require("fs");
 
 const usage = "\nUsage: tran <file_path> file to be used";
 const options = yargs  
@@ -11,4 +12,10 @@ const options = yargs
 
 const path = process.cwd() + "/" + yargs.argv.p
 
-console.log(path);
+console.log(fs.stat(path, (err, stats) => {
+    if(err){
+        console.log(err)
+        return;
+    }
+    console.log(stats)
+}));
